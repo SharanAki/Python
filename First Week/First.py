3a329bde43025689230ae3b7aa2ce71a4249fdb2
Aakarsh <87859248+SharanAki@users.noreply.github.com>
2022-12-28 17:04:02 +0530

    ok

--- First Week/First.py
+++ First Week/First.py
@@ -1,1 +1,13 @@
 print("Hello World!!!!")
+mystring = 'Hello world'
+print(mystring)
+# Hello World First code
+
+import numpy as np
+import matplotlib.pyplot as plt
+t = np.arange(0, 4, 0.1)
+plt.plot(t,t,t,t+2,t,t**2)
+plt.show()
+# Example plot code
+
+# Path: First Week\First.py 
