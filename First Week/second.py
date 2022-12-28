0ae36d2ed749c00483e44cb99e09d9f7a0c5bb85
Aakarsh <87859248+SharanAki@users.noreply.github.com>
2022-12-28 18:54:55 +0530

    ok

---
+++ First Week/second.py
@@ -0,0 +1,5 @@
+surname = input('Input your surname: ')
+firstname = input('Input your firstname: ')
+print('Your surname is:', surname)
+print('Your firstname is:', firstname)
+print('Your full name is:', surname, firstname)
