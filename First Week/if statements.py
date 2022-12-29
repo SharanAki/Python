d6f42813b51264b219571e5b09a13a840621499e
Aakarsh <87859248+SharanAki@users.noreply.github.com>
2022-12-29 21:22:18 +0530

    1.2

---
+++ First Week/if statements.py
@@ -0,0 +1,7 @@
+sd1 = int(input("First side :"))
+sd2 = int(input("Second side :"))
+
+if sd1 == sd2:
+    print("The Square's area is", sd1*sd2)
+else:
+    print("The rectangle's area is", sd1*sd2)
