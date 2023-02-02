93edc6ccb12287ebf5cfa98bf1ecf1906b305050
Aakarsh <87859248+SharanAki@users.noreply.github.com>
2023-02-02 21:12:07 +0530

    ok

--- Second Week/Second.py
+++ Second Week/Second.py
@@ -1,17 +1,5 @@
-#To draw heart using turtle
-import turtle   
-t = turtle.Pen()
-t.speed(0)
-t.pencolor("red")
-t.fillcolor("pink")
-t.begin_fill()
-t.left(140)
-t.forward(180)
-t.circle(-90,200)
-t.setheading(60)
-t.circle(-90,200)
-t.forward(180)
-t.end_fill()
-t.hideturtle()
-turtle.done()
-    
+f = open('data1.txt','w')
+f.write("GOOD MORNING")
+f.close()
+f = open('data1.txt')
+print(f.read())
